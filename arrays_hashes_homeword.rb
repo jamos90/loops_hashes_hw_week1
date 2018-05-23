879dc06aacc78d587af5a07973fba31663f94cf5
jamos90 <jamos_98@hotmail.com>
2018-05-23 20:39:13 +0100

    exercise 3finished

--- arrays_hashes_homeword.rb
+++ arrays_hashes_homeword.rb
@@ -190,6 +190,8 @@ united_kingdom = [
 
 #1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 
+united_kingdom[1][:capital] = "Cardiff"
+p united_kingdom[1][:capital]
 
 
 #2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
@@ -204,7 +206,7 @@ p united_kingdom
 #3. Use a loop to print the names of all the countries in the UK.
 
    for country in united_kingdom
-  p "The countries in the UK are #{:name}"
+  p "The countries in the UK are #{country[:name]}"
 end
 
 
