a61e1d782b4b9cbdbc327cd991b2b57f7d521984
jamos90 <jamos_98@hotmail.com>
2018-05-23 16:38:48 +0100

    excerise b first commit

--- arrays_hashes_homeword.rb
+++ arrays_hashes_homeword.rb
@@ -2,62 +2,62 @@
 
 # Given the following data structure:
 
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-
-# Complete these tasks:
-
-#1. Add `"Edinburgh Waverley"` to the end of the array
-
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+#
+# # Complete these tasks:
+#
+# #1. Add `"Edinburgh Waverley"` to the end of the array
+#
 # stops.push("Edinbrugh")
 # p stops
-
-#2. Add `"Glasgow Queen St"` to the start of the array
-
+#
+# #2. Add `"Glasgow Queen St"` to the start of the array
+#
 # stops.insert(0, "Glasgow")
 # p stops
-
-#3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-
+#
+# #3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+#
 # stops.insert(4, "Polmont")
 # p stops
-
-#4. Work out the index position of `"Linlithgow"
-
+#
+# #4. Work out the index position of `"Linlithgow"
+#
 # p stops.index("Linlithgow")
-
-#5. Remove `"Livingston"` from the array using its name
-
+#
+# #5. Remove `"Livingston"` from the array using its name
+#
 # stops.delete("Livingston")
 # p stops
-
-#6. Delete `"Cumbernauld"` from the array by index
-
+#
+# #6. Delete `"Cumbernauld"` from the array by index
+#
 # stops.delete_at(1)
 # p stops
-
-#7. How many stops there are in the array?
-
+#
+# #7. How many stops there are in the array?
+#
 # p stops.length
-
-#8. How many ways can we return `"Falkirk High"` from the array?
-
-p stops[2]
-p stops[-4]
-for stations in stops
-  if stations == "Falkirk High"
-    p stations
-  end
-end
-p stops.select {|n| n=="Falkirk High" }
-p stops.fetch(2)
-p stops.fetch(-4)
-
-#9. Reverse the positions of the stops in the array
-
+#
+# #8. How many ways can we return `"Falkirk High"` from the array?
+#
+# p stops[2]
+# p stops[-5]
+# for stations in stops
+#   if stations == "Falkirk High"
+#     p stations
+#   end
+# end
+# p stops.select {|n| n=="Falkirk High" }
+# p stops.fetch(2)
+# p stops.fetch(-5)
+#
+# #9. Reverse the positions of the stops in the array
+#
 # p stops.reverse()
-
-#10. Print out all the stops using a for loop
-
+#
+# #10. Print out all the stops using a for loop
+#
 # for stations in stops
 #  p stations
 # end
@@ -125,6 +125,7 @@ users = {
 # Complete these tasks:
 
 #1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+ p users["Erik"][:twitter]
 #2. Get Erik's hometown
 #3. Get the array of Erik's lottery numbers
 #4. Get the type of Avril's pet Monty
